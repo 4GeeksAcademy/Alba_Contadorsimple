884e456cc37f98f0a3ba2f4c46d0d74364adea50
Alba2308 <Albahb23@hotmail.com>
2026-04-09 03:20:22 +0000

    eliminacion de UseState

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,36 +1,33 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
-
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
 
-
 import '../styles/index.css';
 
-// componente SecondsCounter
+// SecondsCounter
 import SecondsCounter from './components/SecondsCounter';
 
+let seconds = 0;
+
+const updateCounter = () => {
+  // la cantidad de segundos cada segundo
+  seconds++;
+  root.render(<App />);
+};
+
+// Inicializaml intervalo
+setInterval(updateCounter, 1000);
+
 const App = () => {
-  // Segundos
-  const [seconds, setSeconds] = useState(0);
-
-  // Usar useEffect
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setSeconds(prevSeconds => prevSeconds + 1);  // Incrementar los segundos cada segundo
-    }, 1000);
-
-    // Limpiar el intervalo cuando el componente se desmonte
-    return () => clearInterval(interval);
-  }, []);  
-
   return (
     <SecondsCounter seconds={seconds} />
   );
 };
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
